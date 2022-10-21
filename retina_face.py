9ed8f3aa2441218bb5e85e95ffde80e169ba737a
joji <jojinattar@gmail.com>
2022-10-21 10:28:20 +0530

    retina_face

--- retina_face.py
+++ retina_face.py
@@ -8,10 +8,13 @@ camera = cv2.VideoCapture(0)
 while True:
     _, img = camera.read()
     t1 = time.time()
-    faces = detector(img)
+    faces = detector(img[:,:,::-1])
     print(time.time()-t1)
     if faces:
         box, landmarks, score = faces[0]
+        print(score)
+        for landmark in landmarks:
+            cv2.circle(img, np.int0(landmark),2,(255,0,0),-1)
         cv2.rectangle(img, np.int0((box[0],box[1])), np.int0((box[2],box[3])),(0,0,255))
     cv2.imshow(" ", cv2.flip(img,1))
     key = cv2.waitKey(1) & 0xFF
